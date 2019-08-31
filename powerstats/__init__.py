0fca6b692b85408a4141db08596ce189cedd3d31
Stephan <erlenmayr@gmail.com>
2019-08-31 17:49:26 +0200

    clockwork with teeth

---
+++ powerstats/__init__.py
@@ -0,0 +1,20 @@
+import display
+import power
+import os
+import utime
+
+def powerstats():
+    with display.open() as disp:
+        while True:
+            volt = os.read_battery()
+            curr = power.read_battery_current() * 1000
+            powr = volt * curr
+            perc = (volt - 3.4) / 0.8 * 100
+            disp.clear()
+            disp.print("{:.1f} %".format(perc), posx=0, posy=0)
+            disp.print("{:.3f} V".format(volt), posx=0, posy=20)
+            disp.print("{:.1f} mA".format(curr), posx=0, posy=40)
+            disp.print("{:.1f} mW".format(powr), posx=0, posy=60)
+            disp.update()
+            utime.sleep_ms(100)
+
--- uhrwerk/__init__.py
+++ uhrwerk/__init__.py
@@ -13,14 +13,28 @@ import power
 
 
 
-def draw_gear(disp, x, y, radius, ang, col):
+def draw_teeth(disp, x, y, radius, ang, col, s):
+    for i in range(round(2.1 * radius)):
+        tang = ang + i * 3 / radius
+        x1 = x - round(radius * math.sin(tang))
+        y1 = y - round(radius * math.cos(tang))
+        x2 = x - round((radius + 1) * math.sin(tang))
+        y2 = y - round((radius + 1) * math.cos(tang))
+        disp.line(x1, y1, x2, y2, col=col, size=s)
+
+
+
+def draw_gear(disp, x, y, radius, ang, col, *, teeth=True):
     xd = round((radius - 5) * math.sin(ang))
     yd = round((radius - 5) * math.cos(ang))
+    if teeth:
+        draw_teeth(disp, x, y, radius, ang, color.BLACK, 2)
     disp.line(x + xd, y + yd, x - xd, y - yd, col=color.BLACK, size=5)
     disp.circ(x, y, radius - 6, col=col, filled=False, size=6)
     disp.circ(x, y, radius - 6, col=color.BLACK, filled=False)
-    disp.circ(x, y, radius, col=color.BLACK, filled=False)
     disp.line(x + xd, y + yd, x - xd, y - yd, col=col, size=4)
+    if teeth:
+        draw_teeth(disp, x, y, radius, ang, col, 1)
 
 
 
@@ -33,31 +47,30 @@ def draw_hand(disp, x, y, len, ang, col, thick):
 
 
 # draws a spring that indicates battery charge
-def draw_spring(disp):
-    volt = os.read_battery()
-    perc = (volt - 3.4) / 0.8
-    f = 14 -  min(10, round(perc * 10))
-    disp.rect(10, 5, 100, 15, col=htmlcolor.DARKRED)
-    disp.rect(100, 5, 160, 15, col=htmlcolor.DARKGREEN)
-    disp.rect(0, 0, 10, 20, col=color.BLACK)
-    disp.rect(1, 1, 9, 19, col=htmlcolor.DIMGRAY)
+def draw_spring(disp, charge):
+    disp.rect(0, 0, 9, 19, col=htmlcolor.DIMGRAY)
+    disp.rect(10, 5, 99, 14, col=htmlcolor.DARKRED)
+    disp.rect(100, 5, 159, 14, col=htmlcolor.DARKGREEN)
+    p = round(90 - 60 * charge)
+    disp.rect(10 + p, 0, 80 + p, 20, col=htmlcolor.BLACK)
+    disp.rect(11 + p, 1, 79 + p, 19, col=htmlcolor.DIMGRAY)
     for i in range(6):
-        disp.line(12+f*i, 2, 12+round(0.5*f)+f*i, 18, col=color.BLACK, size=2)
-        disp.line(12+f*i, 2, 12+round(0.5*f)+f*i, 18, col=htmlcolor.DIMGRAY)
-    disp.rect(15+round(0.5*f)+f*i, 0, 90+f*i, 20, col=color.BLACK)
-    disp.rect(16+round(0.5*f)+f*i, 0, 89+f*i, 19, col=htmlcolor.DIMGRAY)
+        f = 10 + round((2 * i + 1)     * (p / 12.0))
+        g = 10 + round((2 * i + 2) * (p / 12.0))
+        disp.line(f, 2, g, 18, col=htmlcolor.BLACK, size=2)
+        disp.line(f, 2, g, 18, col=htmlcolor.DIMGRAY)
 
 
 
 def draw_gears(disp, t, ms):
     # pendel
-    draw_gear(disp, 90, 39, 50, 1.570796 - 1.047198  * math.sin(0.006283185 * ms), htmlcolor.DARKGOLDENROD)
+    draw_gear(disp, 90, 39, 50, 1.570796 - 1.047198  * math.sin(0.006283185 * ms), htmlcolor.DARKGOLDENROD, teeth=False)
     # minute gears
     draw_gear(disp, 80, 40, 35, t[4] * -0.1047198, htmlcolor.DIMGRAY)
-    draw_gear(disp, 133, 50, 20, t[4] * 0.3665192, htmlcolor.DIMGRAY)
+    draw_gear(disp, 136, 50, 20, t[4] * 0.3665192, htmlcolor.DIMGRAY)
     # second gears
     draw_gear(disp, 80, 40, 15, t[5] * -0.1047198, htmlcolor.DIMGRAY)
-    draw_gear(disp, 38, 54, 30, t[5] * 0.05235988, htmlcolor.DIMGRAY)
+    draw_gear(disp, 33, 46, 30, t[5] * 0.05235988, htmlcolor.DIMGRAY)
 
 
 
@@ -79,11 +92,16 @@ def animate():
             t = utime.localtime()
             ms = utime.time_ms() % 1000
             disp.clear()
-            draw_spring(disp)
+            volt = os.read_battery()
+            charge = (volt - 3.4) / 0.8
+            t0 = utime.time_ms()
+            draw_spring(disp, charge)
             draw_gears(disp, t, ms)
             draw_hands(disp, t)
+            t1 = utime.time_ms()
             disp.update()
-            utime.sleep_ms(20)
+            disp.print(str(t1-t0), posx=100, posy=60)
+            disp.update()
 
 
 
